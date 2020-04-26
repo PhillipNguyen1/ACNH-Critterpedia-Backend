5b776a2464309f01a6ced308d79fbbe4b7068abd
Phillip Nguyen <phillip.n@slalom.com>
2020-04-26 01:21:10 -0500

    Added integration test for GET /

--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
-var assert = require('chai').assert;
+const assert = require('chai').assert;
+var request = require('supertest');
+var app = require('../app.js');
 
+// Unit test example
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
@@ -7,5 +10,13 @@ describe('Array', function () {
       const isFound = arr.indexOf(4);
       assert(isFound, -1);
     });
+  });
+});
+
+// Integration test example. Requires application to be built first to test
+describe('GET /', function () {
+  it('respond with Hello World!', function (done) {
+    // navigate to root and check the response is 'hello world'
+    request(app).get('/').expect('Hello World!', done);
   });
 });
